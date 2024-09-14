8fb3e623efe147aa859509a610f1884b8ee7d136
Icantfindaname05 <oliversboehm@gmail.com>
2024-09-14 12:14:51 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,19 +10,22 @@ sudo apt update
 sudo apt install papirus-icon-theme
 sudo add-apt-repository --remove  ppa:papirus/papirus
 #install visule studio code
-#instructions from https://code.visualstudio.com/docs/setup/linux
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code 
-#remove hexchat and parole media player
+#instructions from https://code.visualstudio.com/docs/setup/linux #### !!!!!!!!! APT INSTALL VSCODE should work lolz
+#sudo apt-get install wget gpg
+#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+#sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+#echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+#rm -f packages.microsoft.gpg
+#sudo apt install apt-transport-https # command to install vscode the long way 
+#sudo apt update
+#sudo apt install code 
+#remove unwanted programs 
 sudo apt remove hexchat parole 
 sudo apt-get purge kdeconnect
 sudo apt update && sudo apt upgrade 
 #git clone - github location of cheet sheet - | add in newer version
-rm -f install.sh #remove installer from computer
+#rm -f install.sh
+#git clone for future cheet sheet downloader
+#cd ~ && cd /Desktop
+#git clone placeholder.url 
 
